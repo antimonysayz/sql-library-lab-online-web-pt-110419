2e4ab4b6467fe08a438a5c06d6e9c38e17d24632
Anthony Mai <anthony.mai530@gmail.com>
2020-01-28 02:54:56 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,13 +1,14 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "select books.title, books.year
-  from books
-  where books.series_id = 1
-  order by books.year;
+  "SELECT books.title, books.year
+  FROM books
+  WHERE books.series_id = 1
+  ORDER BY books.year;
 "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto
+  FROM"
 end
 
 
