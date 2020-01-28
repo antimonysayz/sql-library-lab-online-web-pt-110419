e23e25a79927b0ad4dc2db0ba38d3df344ed5d31
Anthony Mai <anthony.mai530@gmail.com>
2020-01-28 03:00:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM characters
+  INNER JOIN series
+  on character.series_id = series.id
+  HAVING characters.species = 'human'
+  ORDER BY COUNT(character.species) DESC
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
